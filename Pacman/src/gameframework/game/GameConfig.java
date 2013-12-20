265233d937dc1157a7c36fd7914fbd0a51096699
Guillaume Guerin De Tourville <gguerind@gmail.com>
2013-12-20 20:38:54 +0100

    redimmensionement fenetre x2 et Ajout classe gameConfig dans gameframework/game/

---
+++ Pacman/src/gameframework/game/GameConfig.java
@@ -0,0 +1,9 @@
+package gameframework.game;
+
+public class GameConfig {
+	public static final int NB_ROWS = 31;
+	public static final int NB_COLUMNS = 28;
+	public static final int SPRITE_SIZE = 32;
+	public static final int MAX_NUMBER_OF_PLAYER = 4;
+	public static final int NUMBER_OF_LIVES = 1;
+}
